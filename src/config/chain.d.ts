0f144214a3e11414fbc2a46becd0dd39fd65b010
Slava Startsev <slava.startsev@tuta.io>
2022-11-16 16:39:48 +0300

    chore: small ajustments

--- src/config/chain.d.ts
+++ src/config/chain.d.ts
@@ -13,6 +13,8 @@ export const CHAIN_ID: Record<ChainName, ChainId> = {
   ETHEREUM: '1',
   RINKEBY: '4',
   VOLTA: '73799',
+  GTON: '1000',
+  GTON_TESTNET: '50021',
 }
 
 // Values match that required of onboard and returned by CGW
